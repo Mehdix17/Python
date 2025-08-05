34330d61b88c631d1ef3de0e14ba48e8d058dcef
Mehdix17 <121356310+Mehdix17@users.noreply.github.com>
2025-08-05 21:58:39 +0100

    Delete Simple directory

--- Simple/Table_de_multiplication.py
+++
@@ -1,9 +0,0 @@
-def mult(j) :
-    for i in range(1,11) :
-        print(f"\n{j} * {i} = {j*i}")
-
-while 1 :
-    x = int(input("\nVeuillez entrer un nombre : "))
-    print("\nLa table de multiplication de",x,": ")
-    mult(x)
-
--- Simple/test.py
+++
@@ -1,6 +0,0 @@
-while 1 :
-    x = int(input("\nEntrer la note : "))
-    if x >= 10 :
-        print("\nAdmis")
-    else :
-        print("\nNos admis")
